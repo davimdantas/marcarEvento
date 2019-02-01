d203cf6524e3fd60d608b981fd74c71847e22d0e
davimdantas <davimdantas@gmail.com>
2019-02-01 18:44:40 -0300

    SelectingDateinSecondView

--- src/app/definyDate/start-view/start-view.component.ts
+++ src/app/definyDate/start-view/start-view.component.ts
@@ -22,7 +22,122 @@ export class StartViewComponent implements OnInit {
   daysForTable = new BehaviorSubject(this.calendarArray);
   firstHour = 7;
   lastHour = 18;
+
+  meses = {
+    Jan: 0,
+    Fev: 1,
+    Mar: 2,
+    Abr: 3,
+    Mai: 4,
+    Jun: 5,
+    Jul: 6,
+    Ago: 7,
+    Set: 8,
+    Out: 9,
+    Nov: 10,
+    Dez: 11
+  };
+
+  firstClick = {
+    hour: 1,
+    hourString: '01:00',
+    day: 0,
+    month: 0,
+    defined: false
+  };
+  secondClick = {
+    hour: 1,
+    hourString: '01:00',
+    day: 0,
+    month: 0,
+    defined: false
+  };
+  hourClickedFirst = 1;
+  elementWithDesireClass;
+  firstElement;
+  secondElement;
+
+  hoveredElements = [];
+
+  hoursToHighlight = [];
   dataSource: Observable<any[]>;
+
+  onDateSelection(date) {
+    const hour = parseInt(date.srcElement.innerHTML.substring(0, 2), 10);
+    const hourString = date.srcElement.innerHTML;
+    const day = parseInt(date.toElement.classList[2].substring(13, 15), 10);
+    const month = this.meses[date.toElement.classList[2].substring(15, 18)];
+    const classes = date.toElement.classList.value;
+    const elements = document.getElementsByClassName(classes);
+    for (const i in elements) {
+      if (elements.hasOwnProperty(i) && elements[i].innerHTML === hourString) {
+        this.elementWithDesireClass = elements[i];
+      }
+    }
+
+    if (this.firstClick.defined === false && hourString.substring(3, 5) === '00') {
+      this.firstClick.month = month;
+      this.firstClick.day = day;
+      this.firstClick.hour = hour;
+      this.firstClick.hourString = hourString;
+      this.firstClick.defined = true;
+      this.firstElement = this.elementWithDesireClass;
+      this.firstElement.classList.add('highlighted');
+    } else if (
+      hour > this.firstClick.hour &&
+      day === this.firstClick.day &&
+      month === this.firstClick.month &&
+      this.secondClick.defined === false
+    ) {
+      this.secondElement = this.elementWithDesireClass;
+      this.secondElement.classList.add('highlighted');
+      this.secondClick.month = month;
+      this.secondClick.day = day;
+      this.secondClick.hour = hour;
+      this.secondClick.hourString = hourString;
+
+      this.secondClick.defined = true;
+    } else if (hourString.substring(3, 5) === '00') {
+      this.removeClass('hovered');
+      this.firstElement.classList.remove('highlighted');
+      this.firstClick.month = month;
+      this.firstClick.day = day;
+      this.firstClick.hour = hour;
+      this.firstClick.hourString = hourString;
+
+      this.elementWithDesireClass.classList.add('highlighted');
+      this.firstElement = this.elementWithDesireClass;
+
+      this.firstClick.defined = true;
+      this.secondClick.defined = false;
+      this.secondElement.classList.remove('highlighted');
+    }
+
+    if (this.firstClick.defined === true && this.secondClick.defined === true) {
+      console.log('\n No IFFFF');
+      this.hoursToHighlight = [];
+
+      for (let hora = this.firstClick.hour + 1; hora < this.secondClick.hour; hora++) {
+        this.hoursToHighlight.push(`${hora < 10 ? '0' + hora : hora}:00`);
+      }
+
+      for (const i in elements) {
+        if (elements.hasOwnProperty(i) && this.hoursToHighlight.includes(elements[i].innerHTML)) {
+          elements[i].classList.add('hovered');
+          this.hoveredElements.push(elements[i]);
+        }
+      }
+    }
+  }
+  removeClass(classToRemove: string) {
+    for (const position in this.hoveredElements) {
+      if (this.hoveredElements.hasOwnProperty(position)) {
+        console.log('\n this.hoveredElements ', this.hoveredElements[position]);
+        this.hoveredElements[position].classList.remove(classToRemove);
+      }
+    }
+    this.hoveredElements = [];
+  }
 
   calendarGenerator(fHour: number, lHour: number) {
     const newObj = {};
@@ -86,10 +201,9 @@ export class StartViewComponent implements OnInit {
     }
   }
 
-
   updateDates() {
-    this.definyDate.getChanges().subscribe( response => {
-      this.objectFromServer =  response;
+    this.definyDate.getChanges().subscribe(response => {
+      this.objectFromServer = response;
       this.firstHour = this.timeStringToInt(this.objectFromServer.firstHour);
       this.lastHour = this.timeStringToInt(this.objectFromServer.lastHour);
       this.allowedDays = [];
@@ -111,10 +225,23 @@ export class StartViewComponent implements OnInit {
       this.dataSource = this.daysForTable.pipe(map(v => Object.values(v)));
     });
   }
+  highlight(element: Element) {
+    element.highlighted = !element.highlighted;
+  }
+
   constructor(public definyDate: DefinyDate) {}
 
   async ngOnInit() {
     this.updateDates();
   }
+}
 
+export interface Element {
+  checked: boolean;
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  highlighted?: boolean;
+  hovered?: boolean;
 }
